57c2d193a67ddfa841b79f100767310ab7fdae7a
LeoKu <czc12580520@gmail.com>
2021-05-10 22:00:40 +0800

    feat: update config

--- src/eslint.ts
+++ src/eslint.ts
@@ -1,48 +1,69 @@
+import fs from 'fs'
+import path from 'path'
+
+const IS_TS_PROJECT = fs.existsSync(
+  path.join(process.cwd() || '.', './tsconfig.json')
+)
+
+const parser = IS_TS_PROJECT
+  ? '@typescript-eslint/parser'
+  : '@babel/eslint-parser'
+
+const _extends = (() => {
+  return [
+    'eslint:recommended',
+    IS_TS_PROJECT ? 'plugin:@typescript-eslint/recommended' : null,
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ].filter(el => el && typeof el === 'string')
+})()
+
+const rules = {
+  'simple-import-sort/imports': 1,
+  'simple-import-sort/exports': 1,
+  'sort-imports': 0,
+  'import/order': 0,
+  'react/jsx-sort-props': [
+    2,
+    {
+      callbacksLast: true,
+      shorthandFirst: true,
+      shorthandLast: false,
+      ignoreCase: false,
+      noSortAlphabetically: false,
+      reservedFirst: true,
+    },
+  ],
+  ...(IS_TS_PROJECT
+    ? {
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-non-null-assertion': 0,
+        '@typescript-eslint/no-use-before-define': 2,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/consistent-type-imports': [
+          2,
+          {
+            prefer: 'type-imports',
+            disallowTypeAnnotations: true,
+          },
+        ],
+      }
+    : {}),
+}
+
 module.exports = {
   env: {
     node: true,
     browser: true,
     es6: true,
   },
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
+  parser,
+  extends: _extends,
   plugins: ['simple-import-sort'],
-  rules: {
-    'simple-import-sort/imports': 1,
-    'simple-import-sort/exports': 1,
-    'sort-imports': 0,
-    'import/order': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/no-use-before-define': 2,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/consistent-type-imports': [
-      2,
-      {
-        prefer: 'type-imports',
-        disallowTypeAnnotations: true,
-      },
-    ],
-    'react/jsx-sort-props': [
-      2,
-      {
-        callbacksLast: true,
-        shorthandFirst: true,
-        shorthandLast: false,
-        ignoreCase: false,
-        noSortAlphabetically: false,
-        reservedFirst: true,
-      },
-    ],
-  },
+  rules,
 }
