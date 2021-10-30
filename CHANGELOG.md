a6e0c7f3868a765cecc3fb66c46b97c60f3ab6bf
LeoKu <czc12580520@gmail.com>
2021-10-31 00:08:18 +0800

    chore: bump v0.4.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [0.4.4, 0.4.5] - 2021-10-30
+
+- 升级 `ESlint` 8.x
+- 升级 `Stylelint` 14.x
+- prettier `bracketSameLine`
+
 ## [0.4.3] - 2021-09-04
 
 - 添加对 `plugin:import/typescript` 的判断
--- src/stylelint.ts
+++ src/stylelint.ts
@@ -15,6 +15,8 @@ module.exports = {
         ignoreAtRules: ['tailwind'],
       },
     ],
+    'selector-class-pattern':
+      '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
   },
 
   ignoreFiles: ['build/**/*.css'],
