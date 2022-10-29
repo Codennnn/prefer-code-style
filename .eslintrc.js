96671e90b6e334c702277190ad2cc70635751abd
LeoKu <czc12580520@gmail.com>
2022-10-29 17:07:45 +0800

    chore: merge eslint ignore file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: [require.resolve('./lib/eslint')],
+  ignorePatterns: [
+    'node_modules'.at,
+    'lib',
+    '!.prettierrc.js',
+    '!.eslintrc.js',
+    '!.stylelintrc.js',
+  ],
 }
