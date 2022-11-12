ddd1a9339b41bc60aafa74d1c79655d983f52922
LeoKu <czc12580520@gmail.com>
2022-11-12 10:38:51 +0800

    chore: update stylelint config

--- src/stylelint.ts
+++ src/stylelint.ts
@@ -1,30 +1,31 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
-    'stylelint-prettier/recommended',
+    'stylelint-config-recommended',
     'stylelint-config-rational-order',
+    'stylelint-prettier/recommended',
   ],
 
-  plugins: ['stylelint-scss', 'stylelint-order'],
+  plugins: ['stylelint-less', 'stylelint-scss', 'stylelint-order'],
 
   rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': [
+    'at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: [
           // 添加对 tailwind 指令的支持
           'tailwind',
           'apply',
+          'variants',
+          'responsive',
+          'screen',
           'layer',
           'import',
-          'screen',
           // ====================
         ],
       },
     ],
-    'color-function-notation': 'legacy',
-    'alpha-value-notation': 'number',
+    'color-function-notation': 'modern',
     'selector-class-pattern':
       '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
   },
