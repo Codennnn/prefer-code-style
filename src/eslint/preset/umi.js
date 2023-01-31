6d636d45abba9a163a7ef99ed70b1fb869e8561d
LeoKu <czc12580520@gmail.com>
2023-01-31 15:42:41 +0800

    feat: make eslint config composable

---
+++ src/eslint/preset/umi.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    require.resolve('./node'),
+    require.resolve('./browser'),
+    require.resolve('./typescript'),
+    require.resolve('./react'),
+    require.resolve('./tailwindcss'),
+  ],
+}
