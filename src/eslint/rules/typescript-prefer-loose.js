970b91749c5cfb2778fa9717886aa68cb99b7f62
LeoKu <czc12580520@gmail.com>
2023-02-24 08:32:57 +0800

    feat

--- src/eslint/rules/typescript-prefer-loose.js
+++ src/eslint/rules/typescript-prefer-loose.js
@@ -3,5 +3,6 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/prefer-readonly-parameter-types': 0,
     '@typescript-eslint/prefer-regexp-exec': 0,
+    '@typescript-eslint/no-floating-promises': 0,
   },
 }
--- src/eslint/rules/typescript-prefer-strict.js
+++ src/eslint/rules/typescript-prefer-strict.js
@@ -3,5 +3,6 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 1,
     '@typescript-eslint/prefer-readonly-parameter-types': 1,
     '@typescript-eslint/prefer-regexp-exec': 0,
+    '@typescript-eslint/no-floating-promises': 1,
   },
 }
