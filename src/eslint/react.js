67eca96277eaee96be3dc610615639e661bee4a3
LeoKu <czc12580520@gmail.com>
2023-05-08 21:03:55 +0800

    feat: remove `eslint-plugin-jsx-falsy`

--- src/eslint/react.js
+++ src/eslint/react.js
@@ -1,4 +1,4 @@
-const { COMMON_SORT_GROUPS, TYPESCRIPT_FILES } = require('../constants')
+const { COMMON_SORT_GROUPS } = require('../constants')
 
 module.exports = {
   extends: [
@@ -33,19 +33,6 @@ module.exports = {
       },
     ],
   },
-  overrides: [
-    {
-      files: TYPESCRIPT_FILES,
-      parser: '@typescript-eslint/parser',
-      plugins: ['jsx-falsy'],
-      rules: {
-        'jsx-falsy/no-falsy-and': 1,
-      },
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
-    },
-  ],
   settings: {
     react: {
       version: 'detect',
