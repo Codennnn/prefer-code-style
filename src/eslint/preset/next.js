0aec88243d12ab73ef0b9e413792be75239ee776
LeoKu <leokudev@gmail.com>
2023-08-17 20:36:20 +0800

    Use strict typescript rules in Next

--- src/eslint/preset/next.js
+++ src/eslint/preset/next.js
@@ -3,6 +3,7 @@ module.exports = {
     require.resolve('../node'),
     require.resolve('../browser'),
     require.resolve('../typescript'),
+    require.resolve('../rules/typescript-prefer-strict'),
     require.resolve('../react'),
     require.resolve('../next'),
     require.resolve('../tailwindcss'),
