5c24dcfc7546591c67945127afef1fdad0d51cc8
LeoKu <leokudev@gmail.com>
2023-08-19 11:18:56 +0800

    Add eslint ts rule

--- src/eslint/typescript.js
+++ src/eslint/typescript.js
@@ -16,6 +16,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 0,
         '@typescript-eslint/no-non-null-assertion': 0,
         '@typescript-eslint/no-use-before-define': 1,
+        '@typescript-eslint/consistent-type-definitions': 1,
         '@typescript-eslint/explicit-module-boundary-types': 0,
         '@typescript-eslint/consistent-type-imports': [1, { fixStyle: 'inline-type-imports' }],
 
