143bd740ddd488833bbeca800fd40e656f117eb9
LeoKu <czc12580520@gmail.com>
2023-09-11 10:36:58 +0800

    feat: add `no-floating-promises`

--- src/eslint/rules/typescript-prefer-strict.js
+++ src/eslint/rules/typescript-prefer-strict.js
@@ -2,7 +2,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-var-requires': 1,
     '@typescript-eslint/prefer-regexp-exec': 0,
-    '@typescript-eslint/no-floating-promises': 1,
+    '@typescript-eslint/no-floating-promises': 0,
     '@typescript-eslint/no-misused-promises': 1,
   },
 }
