b6540e70f0b7b238f81bbb4bd92ab91030f94af3
LeoKu <czc12580520@gmail.com>
2023-09-11 10:27:38 +0800

    Add `no-floating-promises`

--- src/eslint/typescript.js
+++ src/eslint/typescript.js
@@ -26,6 +26,7 @@ module.exports = {
 
         '@typescript-eslint/dot-notation': 0,
         '@typescript-eslint/no-unsafe-assignment': 0,
+        '@typescript-eslint/no-floating-promises': 0,
         '@typescript-eslint/restrict-template-expressions': 2,
         '@typescript-eslint/prefer-nullish-coalescing': 0, // Track this related issue: https://github.com/typescript-eslint/typescript-eslint/issues/4906
       },
