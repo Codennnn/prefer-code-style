61593d43e878f4a23a352d98a40b644d17b60e86
LeoKu <leokudev@gmail.com>
2024-05-14 10:34:37 +0800

    Update Stylelint

--- src/stylelint.js
+++ src/stylelint.js
@@ -1,13 +1,30 @@
+const { propertyGroups } = require('stylelint-config-clean-order')
+
+const propertiesOrder = propertyGroups.map((properties) => ({
+  noEmptyLineBetween: true,
+  emptyLineBefore: 'never', // Don't add empty lines between order groups.
+  properties,
+}))
+
 module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recommended',
-    // 'stylelint-prettier/recommended',
+    'stylelint-config-clean-order',
+    'stylelint-prettier/recommended',
   ],
 
   plugins: ['stylelint-less', 'stylelint-scss', 'stylelint-order'],
 
   rules: {
+    'order/properties-order': [
+      propertiesOrder,
+      {
+        severity: 'warning',
+        unspecified: 'bottomAlphabetical',
+      },
+    ],
+
     'at-rule-no-unknown': [
       true,
       {
@@ -24,7 +41,9 @@ module.exports = {
         ],
       },
     ],
+
     'color-function-notation': 'modern',
+
     'selector-class-pattern':
       '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
   },
